f87e2c3ab2c48ca663c0bba6ee352b762246bbf5
iq2gq <abbafidow@gmail.com>
2026-08-06 19:59:46 +1200

    Update pickAssistant.js

--- pickAssistant.js
+++ pickAssistant.js
@@ -1,1 +1,12 @@
+// ========================================
+// IQ2GQ Pick Assistant
+// ========================================
+
+// Turn the Pick Assistant on or off.
+const ENABLE_PICK_ASSISTANT = false;
+
 console.log("Pick Assistant loaded");
+
+if (!ENABLE_PICK_ASSISTANT) {
+    console.log("Pick Assistant is disabled.");
+}
