e17d90462516bd32b2a1960b5d23e429d0baf639
Asif Fisa <abbafidow@gmail.com>
2026-08-12 22:33:38 +1200

    Fix current form syntax

--- app.js
+++ app.js
@@ -972,8 +972,7 @@ function currentFormCard(data) {
   return {
     label: 'Current form',
     value: top.member,
-    detail: `${top.wins}/${top.picks} wins in last ${top.picks} picks (${pct(top.success)})`
-  };
+    detail: `${top.wins}/${top.picks} wins in last ${top.picks} picks (${pct(top.success)})`  };
 }function highConfidenceCard(data) {
   const sportRows = aggregate(data, 'group')
     .filter(x => x.picks >= 100)
